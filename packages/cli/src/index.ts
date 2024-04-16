41ff2da5b74218b950c1d1bbe724af415a56180b
lexmin0412 <zhangle_dev@outlook.com>
2024-04-17 00:44:30 +0800

    feat(packages/cli): 新增分步执行日志输出

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,6 +8,7 @@ const pkgJson = JSON.parse(content)
 // 扫描目录获取 packages 列表
 const packages = fs.readdirSync(path.resolve(process.cwd(), './packages'))
 
+console.log('[info] 生成基本信息 - 开始')
 let readmeLines = [
 	// 标题
   `# ${pkgJson.name}`,
@@ -16,9 +17,12 @@ let readmeLines = [
 	`${pkgJson.description}`,
 	''
 ]
+console.log(`[info] 生成基本信息 - 结束
+`)
 
 // 根据 列表生成 README.md 中的信息
 
+console.log('[info] 生成子包信息 - 开始')
 if (packages?.length) {
 	readmeLines = [
 		...readmeLines,
@@ -37,9 +41,12 @@ packages.forEach(item => {
 	const subPkgJson = JSON.parse(fs.readFileSync(path.resolve(subDir, 'package.json')).toString())
 	readmeLines.push(`|[${subPkgJson.name}](https://www.npmjs.com/package/${subPkgJson.name})|![version](https://img.shields.io/npm/v/${subPkgJson.name})  ![downloads-month](https://img.shields.io/npm/dm/${subPkgJson.name})|${subPkgJson.description}|`)
 })
+console.log(`[info] 生成子包信息 - 结束
+`)
 
 // 追加 Star History
 
+console.log('[info] 生成 Star History - 开始')
 // 获取 git 用户名 + 仓库名
 const reg = /git\+https:\/\/github\.com\/(.*?)\.git/g
 // 替换为 https://github.com/lexmin0412/readue
@@ -59,6 +66,8 @@ if (gitUrl) {
 } else {
 	console.warn('[Warning] 当前 package.json 中没有配置完整的 repository 字段，将跳过 Star History 生成')
 }
+console.log(`[info] 生成 Star History - 结束
+`)
 
 
 // 写到 当前目录的 README.md 中
