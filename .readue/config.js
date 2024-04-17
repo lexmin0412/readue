112996efe5ed390b2845e4c95abb45138d45a9c3
lexmin0412 <zhangle_dev@outlook.com>
2024-04-17 18:17:14 +0800

    feat: 添加 .readue 配置, 文档添加配置说明

---
+++ .readue/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	/**
+	 * 写入模式 cover-覆盖全内容 insert-在占位符处插入内容
+	 */
+	mode: 'insert',
+	/**
+	 * 插入占位符
+	 */
+	insertPlaceholder: '__READUE_PLACEHOLDER__',
+	/**
+	 * 模板文件 - 相对路径
+	 */
+	templateFile: './template.md',
+	/**
+	 * 输出文件路径 - 相对路径
+	 */
+	outputFile: './../README.md',
+}
