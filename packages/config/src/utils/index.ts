7d62fad0390d5496dce117230099d283bab000e1
lexmin0412 <zhangle_dev@outlook.com>
2024-04-20 02:45:56 +0800

    feat(config): 新增 getConfig 函数, 支持获取配置内容

--- packages/config/src/utils/index.ts
+++ packages/config/src/utils/index.ts
@@ -1,4 +1,6 @@
+import * as fs from 'fs'
 import * as path from 'path'
+import { SUPPORTED_FILE_NAMES } from '../constants'
 import { ReadueConfig } from "../types"
 
 /**
@@ -11,4 +13,21 @@ export const getDefaultConfig = (cwd: string = process.cwd()): ReadueConfig => {
 		templateFile: path.resolve(cwd, "./readue/template.md"),
 		outputFile: path.resolve(cwd, "./README.md"),
 	}
+}
+
+/**
+ * 获取配置
+ */
+export const getConfig = (cwd: string = process.cwd()): ReadueConfig => {
+	const checkFiles = SUPPORTED_FILE_NAMES.map((item) => {
+		return path.resolve(process.cwd(), './.readue', item)
+	})
+
+	const configFilePath = checkFiles.find((item) => {
+		return fs.existsSync(item)
+	})
+	if (!configFilePath) {
+		return getDefaultConfig()
+	}
+	return require(configFilePath)
 }
